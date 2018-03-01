f969add6101a1fdb2b02cb03fb3a655fffb062cf
shashravi9 <Onti9S@laga1>
2018-03-01 01:41:45 -0500

    Added Form Control sample

--- WebApplication1/ClientApp/app/app.shared.module.ts
+++ WebApplication1/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { ConsumerFormComponent } from './components/form/form.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        ConsumerFormComponent,
+        HomeComponent        
     ],
     imports: [
         CommonModule,
@@ -26,6 +28,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
+            { path: 'consumer-form', component: ConsumerFormComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
---
+++ WebApplication1/ClientApp/app/components/form/form.component.ts
@@ -0,0 +1,36 @@
+﻿import { Component } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar'
+
+@Component({
+    selector: 'consumer-form',
+    templateUrl: './form.component.html'
+})
+export class ConsumerFormComponent {
+    public minValue: number = 0;
+    public maxValue: number = 0;
+    public calendar: Date;
+
+    checkMin(val: number) {
+        if (val < 0) {
+            this.minValue = 0
+        }
+        
+        else if (val < this.maxValue && this.maxValue > 0) {
+            this.minValue = val;
+        }
+        else {
+            this.maxValue = val;
+        }
+    }
+
+    checkMax(val: number) {
+        if (val < this.minValue) {
+            this.maxValue = this.minValue;
+            console.log(this.maxValue);
+        }
+    }
+
+    save() {
+
+    }
+}
